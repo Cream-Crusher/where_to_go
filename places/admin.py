5461f5825a82a9945cf7887e875c12af1bb81605
Ruk228 <icecreamcrusher21@gmail.com>
2023-04-14 14:42:41 +0400

    del trash and clear media file

--- places/admin.py
+++ places/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
-from places.models import Place, Image
 from django.utils.safestring import mark_safe
-from adminsortable2.admin import SortableAdminMixin
+from adminsortable2.admin import SortableAdminMixin, SortableStackedInline
+
+from places.models import Place, Image
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+
+
+class ImageStackedInline(SortableStackedInline):  # TODO чТО ТО С ЭТИМ СДЕЛТАЬ
+    model = Image
+    raw_id_fields = ("post",)
 
 
 @admin.register(Place)
@@ -20,6 +26,7 @@ class ImageAdmin(SortableAdminMixin, admin.ModelAdmin):
     raw_id_fields = ['post', ]
     list_filter = ['post', ]
     fields = ['img', 'post', 'image', ]
+    #inlines = [ImageStackedInline, ]
 
     def image(self, Image):
         return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
