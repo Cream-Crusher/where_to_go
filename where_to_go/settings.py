70c090941cf6fa9e8e1f6ec340a4b025c4558c53
Ruk228 <icecreamcrusher21@gmail.com>
2023-06-11 18:34:03 +0400

    update readme(Переменные окружения)

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = env('STATIC_URL', '/static/')
+STATIC_URL = env('/STATIC_URL/', '/static/')
 
 MEDIA_URL = env('MEDIA_URL', '/media/')
 
