e4ee01857f7630ca07a9386f0d01abe11bb6ac06
Ruk228 <icecreamcrusher21@gmail.com>
2023-06-14 13:43:58 +0400

    add logger to(response_place==None)

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -1,8 +1,12 @@
 import requests
+import logging
 
 from ...models import Place, Image
 from django.core.management.base import BaseCommand
 from django.core.files.base import ContentFile
+
+logger = logging.getLogger(__file__)
+logging.basicConfig(level=logging.INFO)
 
 
 class Command(BaseCommand):
@@ -12,27 +16,30 @@ class Command(BaseCommand):
         parser.add_argument('poll_ids', nargs='?', type=str)
 
     def handle(self, *args, **options):
-        response_place = requests.get(options['poll_ids'])
+        try:
+            response_place = requests.get(options['poll_ids'])
+            raw_place = response_place.json()
+            place_coordinates = raw_place['coordinates']
 
-        raw_place = response_place.json()
-        place_coordinates = raw_place['coordinates']
+            place, created = Place.objects.get_or_create(
+                title=raw_place['title'],
+                description_short=raw_place['description_short'],
+                description_long=raw_place['description_long'],
+                low=place_coordinates['lng'],
+                lat=place_coordinates['lat'],
+            )
 
-        place, created = Place.objects.get_or_create(
-            title=raw_place['title'],
-            description_short=raw_place['description_short'],
-            description_long=raw_place['description_long'],
-            low=place_coordinates['lng'],
-            lat=place_coordinates['lat'],
-        )
+            if not created:
 
-        if not created:
+                for image in raw_place['imgs']:
+                    image, created = Image.objects.get_or_create(
+                        post=place,
+                        img=image
+                    )
+                    img_link = ContentFile(requests.get(image).content)
+                    img_name = str(image).split('/')[-1]
 
-            for image in raw_place['imgs']:
-                image, created = Image.objects.get_or_create(
-                    post=place,
-                    img=image
-                )
-                img_link = ContentFile(requests.get(image).content)
-                img_name = str(image).split('/')[-1]
+                    image.img.save(img_name, img_link, save=True)
 
-                image.img.save(img_name, img_link, save=True)
+        except requests.exceptions.MissingSchema:
+            logger.info('load_place не получил обязательный параметр')
