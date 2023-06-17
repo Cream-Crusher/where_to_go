a3db280c9dc556a4f48dc42a6821b9fd3fe0f96b
Ruk228 <icecreamcrusher21@gmail.com>
2023-06-17 15:30:48 +0400

    add Mixin(admin.py)

--- places/admin.py
+++ places/admin.py
@@ -5,20 +5,17 @@ from adminsortable2.admin import SortableAdminMixin, SortableStackedInline, Sort
 from places.models import Place, Image
 
 
-def preview(self, image):
+def preview(image):
     image_url = image.img.url
     height = 200
 
-    return format_html('<img src="{}" height={} />', image_url, height)
+    return format_html(f'<img src="{image_url}" height={height} />')
 
 
 class ImageStackedInline(SortableStackedInline):
     model = Image
     raw_id_fields = ['post', ]
-    readonly_fields = ['preview', ]
-
-    def preview(self, image):
-        return preview(self, image)
+    readonly_fields = [preview, ]
 
 
 @admin.register(Place)
@@ -29,10 +26,7 @@ class PlaceAdmin(SortableAdminBase, admin.ModelAdmin):
 
 @admin.register(Image)
 class ImageAdmin(SortableAdminMixin, admin.ModelAdmin):
-    readonly_fields = ['preview', ]
+    readonly_fields = [preview, ]
     list_display = ['post', 'img', ]
     raw_id_fields = ['post', ]
-    fields = ['img', 'post', 'preview', ]
-
-    def preview(self, image):
-        return preview(self, image)
+    fields = ['img', 'post', preview, ]
