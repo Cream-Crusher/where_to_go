e9cf2eeb2b1f4f3fa7b553f2411af665e96dd53c
Ruk228 <icecreamcrusher21@gmail.com>
2023-06-30 16:36:42 +0400

    split the code into two functions(handle).

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -23,6 +23,33 @@ def check_redirect(response):
         raise requests.HTTPError(history)
 
 
+def get_created_place(raw_place):
+    place_coordinates = raw_place['coordinates']
+    img_name = raw_place['title']
+    description_short = raw_place.get('description_short', '')
+    description_long = raw_place.get('description_long', '')
+
+    return Place.objects.get_or_create(
+        title=img_name,
+        lon=place_coordinates['lng'],
+        lat=place_coordinates['lat'],
+        defaults={
+            'description_short': description_short,
+            'description_long': description_long,
+        }
+    )
+
+
+def add_img_to_place(place, response):
+    img_name = place.title
+    img_link = ContentFile(response.content)
+    image = Image.objects.create(
+        post=place,
+        img=f'media/{img_name}'
+    )
+    image.img.save(img_name, img_link, save=True)
+
+
 class Command(BaseCommand):
     help = 'Load location from json link'
 
@@ -33,31 +60,14 @@ class Command(BaseCommand):
         try:
             response_place = get_response(options['poll_ids'])
             raw_place = response_place.json()
-            place_coordinates = raw_place['coordinates']
-            img_name = raw_place['title']
-            description_short = raw_place.get('description_short', '')
-            description_long = raw_place.get('description_long', '')
-            place, created = Place.objects.get_or_create(
-                title=img_name,
-                lon=place_coordinates['lng'],
-                lat=place_coordinates['lat'],
-                defaults={
-                    'description_short': description_short,
-                    'description_long': description_long,
-                }
-            )
+            place, created = get_created_place(raw_place)
 
             if not created:
                 return
 
             for image in raw_place['imgs']:
                 response = get_response(image)
-                img_link = ContentFile(response.content)
-                image = Image.objects.create(
-                    post=place,
-                    img=f'media/{img_name}'
-                )
-                image.img.save(img_name, img_link, save=True)
+                add_img_to_place(place, response)
 
         except Exception as err:
             logger.warning(err)
